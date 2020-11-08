574f48b31f72a2f6243d521bd1b62e1e7c567d99
geborskimateusz <mateuszgeborski@gmail.com>
2020-11-08 13:26:51 +0100

    working on containerization

--- auth/server.go
+++ auth/server.go
@@ -1,17 +1,30 @@
 package auth
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
+// AuthServer is a HTTP interface for authentication.
 type AuthServer struct {
 	http.Handler
 }
 
+const jsonContentType = "application/json"
+
+// NewAuthServer creates a AuthServer with routing configured.
 func NewAuthServer() (*AuthServer, error) {
 	a := new(AuthServer)
 
 	router := http.NewServeMux()
+	router.Handle("/test", http.HandlerFunc(a.testHandler))
 
 	a.Handler = router
 
 	return a, nil
+}
+
+func (a AuthServer) testHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", jsonContentType)
+	json.NewEncoder(w).Encode("this is a test")
 }
