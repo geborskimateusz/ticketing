c2fdc7779353e5d17dbdbe3070ef9b06b461cf5f
geborskimateusz <mateuszgeborski@gmail.com>
2020-11-10 17:55:19 +0100

    fixed dockerfile, working on signup

--- auth/controllers/signup.go
+++ auth/controllers/signup.go
@@ -1,11 +1,20 @@
 package controllers
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Signup(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"data": "Called /api/users/signup"})
+	jsonData, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		// retus error
+	}
+
+	fmt.Printf("%s", string(jsonData))
+	c.JSON(http.StatusOK, c)
 }
--- auth/main.go
+++ auth/main.go
@@ -1,4 +1,4 @@
-package auth
+package main
 
 import (
 	"github.com/geborskimateusz/auth/controllers"
