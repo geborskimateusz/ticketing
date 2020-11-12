173ff539b3b7aff4e20fac4ca332b3e329fb820b
geborskimateusz <mateuszgeborski@gmail.com>
2020-11-12 18:01:19 +0100

    fixed integration tests

--- auth/server/entity/user.go
+++ auth/server/entity/user.go
@@ -2,8 +2,8 @@ package entity
 
 // User is representation of model used to signin or signup
 type User struct {
-	Username        string `json:"username" validate:"required"`
-	Email           string `json:"email"  validate:"email"`
-	Password        string `json:"password" validate:"min=8,max=32,alphanum"`
-	ConfirmPassword string `json:"confirm_password" validate:"eqfield=Password,required"`
+	// Username        string `json:"username" validate:"required"`
+	Email    string `json:"email"  validate:"email"`
+	Password string `json:"password" validate:"min=8,max=32,alphanum"`
+	// ConfirmPassword string `json:"confirm_password" validate:"eqfield=Password,required"`
 }
