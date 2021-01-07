ce9df6118cc2297b3f10e98012518eaf8ef10cf2
geborskimateusz <mateuszgeborski@gmail.com>
2021-01-07 19:01:38 +0100

    working on https

--- auth/main.go
+++ auth/main.go
@@ -3,5 +3,7 @@ package main
 import "github.com/geborskimateusz/auth/api"
 
 func main() {
-	api.Instance().Run()
+	// api.Instance().Run()
+	router := api.Instance()
+	router.RunTLS(":8080", "./fixtures/testdata/cert.pem", "./fixtures/testdata/key.pem")
 }
--- scripts/tests/user-signup.sh
+++ scripts/tests/user-signup.sh
@@ -2,9 +2,9 @@
 
 EMAIL=$(source ./random_email_generator.sh)
 echo "$EMAIL"
-curl -c, --cookie-jar ./cookie.txt --insecure --header "Content-Type: application/json" \
+curl -b ./cookie.txt --header "Content-Type: application/json" \
   --request POST \
   --data '{"email":"'"$EMAIL"'","password":"validPassword"}' \
- https:/ticketing.dev/api/users/signup 
+ https://ticketing.dev/api/users/signup 
 
  #http://localhost:8080/api/users/signup
