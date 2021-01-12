053ec5b1ac1824743e0008f534cc38c8ae7a9145
geborskimateusz <mateuszgeborski@gmail.com>
2021-01-12 15:52:10 +0100

    working on https

--- auth/main.go
+++ auth/main.go
@@ -5,5 +5,5 @@ import "github.com/geborskimateusz/auth/api"
 func main() {
 	// api.Instance().Run()
 	router := api.Instance()
-	router.RunTLS(":8080", "./certs/server.crt", "./certs/server.key")
+	router.RunTLS(":8080", "./certs/ticketing.crt", "./certs/ticketing.key")
 }
