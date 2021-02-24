21e3415bfb88ea40d9e93e702e42f701e4cfed59
geborskimateusz <mateuszgeborski@gmail.com>
2021-02-24 19:27:07 +0100

    fixed subscriber issue

--- nats-test/listener.go
+++ nats-test/listener.go
@@ -3,23 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
+	"runtime"
 	"time"
 
 	stan "github.com/nats-io/stan.go"
 )
 
 func main() {
-	sc, err := stan.Connect("areyouthere", "124", stan.NatsURL("http://localhost:4222"))
+	sc, err := stan.Connect("areyouthere", "124", stan.NatsURL("nats://localhost:4222"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	aw, _ := time.ParseDuration("60s")
-	for {
-		_, err = sc.Subscribe("areyouthere:chat-invitation", func(m *stan.Msg) {
-			m.Ack()
-			fmt.Printf("Received a message: %s\n", string(m.Data))
-		}, stan.SetManualAckMode(), stan.AckWait(aw))
-	}
 
+	messageNum := 0
+	_, err = sc.Subscribe("areyouthere:chat", func(m *stan.Msg) {
+		m.Ack()
+		messageNum++
+		fmt.Printf("Received a message %d: %s\n", messageNum, string(m.Data))
+	}, stan.SetManualAckMode(), stan.AckWait(aw))
+
+	runtime.Goexit()
 }
--- nats-test/publisher.go
+++ nats-test/publisher.go
@@ -2,27 +2,33 @@ package main
 
 import (
 	"log"
+	"time"
 
 	stan "github.com/nats-io/stan.go"
 )
 
 func main() {
-	sc, err := stan.Connect("areyouthere", "123", stan.NatsURL("http://localhost:4222"))
+	sc, err := stan.Connect("areyouthere", "123",
+		stan.NatsURL("nats://localhost:4222"),
+		stan.MaxPubAcksInflight(1000))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ackHandler := func(ackedNuid string, err error) {
-		if err != nil {
-			log.Printf("Warning: error publishing msg id %s: %v\n", ackedNuid, err.Error())
+	ackHandler := func(ackedNuid string, er error) {
+		if er != nil {
+			log.Printf("Warning: error publishing msg id %s: %v\n", ackedNuid, er.Error())
 		} else {
 			log.Printf("Received ack for msg id %s\n", ackedNuid)
 		}
 	}
 
-	_, err = sc.PublishAsync("areyouthere:chat-invitation", []byte("Hello World"), ackHandler)
-	if err != nil {
-		log.Fatal(err)
+	for {
+		time.Sleep(5 * time.Second)
+		_, err = sc.PublishAsync("areyouthere:chat", []byte("Hello World"), ackHandler)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 }
